b160dc572ccba21b4be660ea86464954c4188001
Ayush Likhar <neontechhead@gmail.com>
2017-09-20 18:36:48 +0530

    initial commit

---
+++ app/Controllers/Http/TaskController.js
@@ -0,0 +1,50 @@
+'use strict'
+const Task = use('App/Models/Task')
+const { validate } = use('Validator')
+
+class TaskController {
+	async index ({ view }) {
+  		const tasks = await Task.all()
+  		return view.render('tasks.index', { tasks: tasks.toJSON() })
+	}
+
+	async store ({ request, response, session }) {
+	  // validate form input
+	  const validation = await validate(request.all(), {
+	    title: 'required|min:3|max:255'
+	  })
+
+	  // show error messages upon validation fail
+	  if (validation.fails()) {
+	    session.withErrors(validation.messages())
+	            .flashAll()
+
+	    return response.redirect('back')
+	  }
+
+	  // persist to database
+	  const task = new Task()
+	  task.title = request.input('title')
+	  await task.save()
+
+	  // Fash success message to session
+	  session.flash({ notification: 'Task added!' })
+
+	  return response.redirect('back')
+	}
+
+	async destroy ({ params, session, response }) {
+	  const task = await Task.find(params.id)
+	  await task.delete()
+
+	  // Fash success message to session
+	  session.flash({ notification: 'Task deleted!' })
+
+	  return response.redirect('back')
+	}
+	
+}
+
+module.exports = TaskController
+
+
---
+++ start/routes.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/guides/routing
+|
+*/
+
+const Route = use('Route')
+
+// Route.on('/').render('welcome')
+
+Route.get('/', 'TaskController.index')
+Route.post('tasks', 'TaskController.store')
+Route.delete('tasks/:id', 'TaskController.destroy')
